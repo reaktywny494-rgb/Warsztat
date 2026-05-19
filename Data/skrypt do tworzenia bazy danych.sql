1915cbe6c7c42f3cf7d2fe2995af2399cf2d2b69
reakt <reakt@LAPTOP-13MU4V7S>
2026-05-19 13:40:28 +0200

    Dodaj pliki projektów.

---
+++ Data/skrypt do tworzenia bazy danych.sql
@@ -0,0 +1,25 @@
+create database warsztat;
+
+use warsztat;
+
+create table Meble(
+	Id int auto_increment primary key,
+    Nazwa varchar(100) not null,
+    Opis varchar(255)
+);
+
+create table Narzedzia(
+	Id int auto_increment primary key,
+    Nazwa varchar(100) not null,
+    Opis varchar(255),
+    Mebel_Id int,
+    
+    constraint FK_Narzedzia_Meble foreign key (Mebel_Id) references Meble(ID) on delete set null on update cascade
+);
+
+insert into Meble(Nazwa, Opis) values ('Stół', 'Duży stół warsztatowy');
+
+insert into Narzedzia(Nazwa, Opis, Mebel_id) values ('Młotek', 'Standardowy młotek', 1);
+insert into Narzedzia(Nazwa, Opis, Mebel_id) values ('Śrubokręt', 'Płaski śrubokręt', 1);
+
+drop database warsztat;
